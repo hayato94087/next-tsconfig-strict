2a66d57210acf47a6a67ed2ec75bf4b123e08c3b
hayato94087 <hayato94087@gmail.com>
2024-03-23 08:22:34 +0900

    feat: strictBindCallApplyを有効化

--- src/lib/strict-bind-call-apply.ts
+++ src/lib/strict-bind-call-apply.ts
@@ -2,5 +2,5 @@ function fn(x: string) {
   x.toUpperCase();
 }
 const x1 = fn.call(undefined, "hello");   // 引数の型がstringで問題はない
-const x2 = fn.call(undefined, true);      // 引数の型がstringでなく、booleanのため型エラーがある
-const x3 = fn.call(undefined, 123456789); // 引数の型がstringでなく、numberのため型エラーがある
+// const x2 = fn.call(undefined, true);      // 引数の型がstringでなく、booleanのため型エラーがある
+// const x3 = fn.call(undefined, 123456789); // 引数の型がstringでなく、numberのため型エラーがある
